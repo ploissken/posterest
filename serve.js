5feab3bbe58eab3f9013c4083365750061288009
bodeaux <ploissken@gmail.com>
2019-06-24 12:56:25 -0300

    fixing csr

--- serve.js
+++ serve.js
@@ -5,3 +5,8 @@ app.use(express.static('build'))
 
 const port = process.env.PORT
 app.listen(port, () => console.log(`Post eletronic rest served @ ${port}`))
+
+app.get('*', function (req, res) {
+  console.log('redirecionando o c')
+  res.redirect('/')
+})
