5ae9dae24adc0e0d61855b3bda644f034a8abe45
bodeaux <ploissken@gmail.com>
2019-07-07 12:48:14 -0300

    trying to load profile at startup

--- src/App.js
+++ src/App.js
@@ -3,8 +3,32 @@ import Router from './components/Router'
 import { Segment } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import Noty from 'notifier'
+import api from 'api'
 
 class App extends Component {
+  componentDidMount() {
+    if(!this.props.login.user || !this.props.login.count) {
+      api('/profile').get().then(data => {
+        console.log('componentDidMount @ SignInGrid')
+        console.log(data)
+        this.props.dispatch({
+          type:'SET_COUNT',
+          count: data.count
+        })
+        this.props.dispatch({
+          type:'USER_LOGIN',
+          user: data.user
+        })
+        this.props.dispatch({
+          type:'SET_FAVORITE',
+          fav: data.favorites
+        })
+      }).catch(oops => {
+        console.log('oops', oops)
+      })
+    }
+  }
+
   render() {
     return (
       <Segment basic inverted={this.props.settings.darkmode}>
@@ -18,6 +42,7 @@ class App extends Component {
 const mapStateToProps = (state) => {
   console.log('hi, my name is state', state)
   return {
+    login: state.login,
     settings: state.settings
   }
 }
