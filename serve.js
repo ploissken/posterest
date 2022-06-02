d7410159c252213e38a33a1b80065146a4389729
Rodurico Sam <ploissken@gmail.com>
2022-06-02 19:48:10 -0300

    fixes serving paths

--- serve.js
+++ serve.js
@@ -1,10 +1,15 @@
 console.log(`metajornal's 🖤 still beats`)
 
 const port = process.env.PORT || 3000
+const path = require('path')
 const express = require('express')
 const app = express()
 
 app.use(express.static('build'))
+
+app.get('/*', function (req, res) {
+ res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.listen(
   port,
