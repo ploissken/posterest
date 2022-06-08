1786aedb651ef2caeda512f16f257e69b6ee4cce
Rodurico Sam <ploissken@gmail.com>
2022-06-08 19:05:29 -0300

    fixing serving

--- serve.js
+++ serve.js
@@ -5,15 +5,15 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-app.use(express.static('build'))
+app.use(express.static('dist'))
 
-app.get('*', function (req, res) {
-  console.log(path.join(__dirname, 'build', 'index.html'))
-  res.set({
-    'Access-Control-Allow-Origin': '*'
-  })
- res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+// app.get('*', function (req, res) {
+//   console.log(path.join(__dirname, 'dist', 'index.html'))
+//   res.set({
+//     'Access-Control-Allow-Origin': '*'
+//   })
+//  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+// })
 
 app.listen(
   port,
